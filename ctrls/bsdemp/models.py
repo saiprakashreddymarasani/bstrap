c0e5fcc3618ecba9d1a7d5d47ab78fec72b20eeb
saiprakashreddymarasani <saiprakashreddymarasani@users.noreply.github.com>
2016-08-16 08:30:35 -0700

    Update models.py

--- ctrls/bsdemp/models.py
+++ ctrls/bsdemp/models.py
@@ -1,18 +1,28 @@
 from __future__ import unicode_literals
 
+from __future__ import unicode_literals
+
 from django.db import models
+from django.forms.extras import widgets
 
 # Create your models here.
 
 
 class Employee(models.Model):
-    name= models.CharField(max_length=100)
+    fname= models.CharField(max_length=100)
+    lname= models.CharField(max_length=100)
     role=models.CharField(max_length=25)
-    age=models.IntegerField( max_length=2)
-    photo=models.CharField(max_length=200)
+    age=models.CharField( max_length=2)
+    photo=models.CharField(max_length=200, null = True)
+    zipcode=models.CharField(max_length=5)
+    birth_date = models.DateField(null=True)
 
     def __str__(self):
-         return self.name
+         return self.fname, self.lname, self.zipcode, self.birth_date
+
+
+
+
 
 
 
